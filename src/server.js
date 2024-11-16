d1c47db6de614c7b1b7b049d1e92923ee5212b05
ProgramadorCaetano <ronaldoecaetano@gmail.com>
2024-11-16 12:50:47 -0300

    update routes
    Update routes e  PORT

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 //const app = require('./app')
 import app from './app.js'
+const port = process.env.PORT || 3001;
 
-app.listen(3001, () => console.log('Server is running at port 3001....'))
+//app.listen(port, '0.0.0.0' () => console.log('Server is running at port 3001....'))
+app.listen(port, '0.0.0.0')
